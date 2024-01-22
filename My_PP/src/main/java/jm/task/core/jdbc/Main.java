ead77d5c7b0a5c7e8dc7e15c8275af324631be39
makevkin <makevkin@mail.ru>
2024-01-22 16:38:09 +0400

    update project

--- My_PP/src/main/java/jm/task/core/jdbc/Main.java
+++ My_PP/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,16 +1,13 @@
 package jm.task.core.jdbc;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes;
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.util.Util;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
-
-        Util.getConnect();
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserService userDao = new UserServiceImpl();
 
         userDao.createUsersTable();
 
